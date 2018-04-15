df464ce2fb6aa82e605bf5dfa85a023d9f5690c3
Mads <mads@stege.eu>
2018-04-15 17:26:08 +0200

    Got parser to work correctly - Somewhat, needs further work.

---
+++ src/JSON2SQL/Parser.java
@@ -0,0 +1,61 @@
+package JSON2SQL;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser {
+
+    public static void main(String[] args) throws Exception {
+
+        //Parsing the source *.json file. Switch to either dist_persons, or dist_locations for best results."
+        JSONObject objPersons = (JSONObject) ((JSONObject) new JSONParser().parse(new FileReader("src/JSON2SQL/dist_persons.json"))).get("Persons");
+
+        HashMap<String, HashMap> persons = new HashMap<>();
+        persons.putAll(objPersons);
+
+        for (Map.Entry<String, HashMap> entryPersons : persons.entrySet()) {
+            String pplRawName = (String) entryPersons.getValue().get("name");
+            String pplMail = (String) entryPersons.getValue().get("email");
+            String pplDesc = (String) entryPersons.getValue().get("description");
+            String pplPictureURL = (String) entryPersons.getValue().get("pictureURL");
+            String pplRole = (String) entryPersons.getValue().get("role");
+            String pplRoomName = (String) entryPersons.getValue().get("roomName");
+
+            System.out.println("(\"" + pplRawName + "\", \"" + pplMail + "\", \"" + pplDesc + "\", \"" + pplPictureURL + "\", \"" + pplRole + "\", \"" + pplRoomName + "\"),");
+        }
+
+        JSONObject objLocations = (JSONObject) ((JSONObject) new JSONParser().parse(new FileReader("src/JSON2SQL/dist_locations.json"))).get("Locations");
+
+        HashMap<String, HashMap> locations = new HashMap<>();
+        locations.putAll(objLocations);
+
+        for (Map.Entry<String, HashMap> entryLocations : locations.entrySet()) {
+            String locName = (String) entryLocations.getValue().get("name");
+            String locDesc = (String) entryLocations.getValue().get("description");
+            String locFloor = (String) entryLocations.getValue().get("floor");
+            String locLandmark = (String) entryLocations.getValue().get("landmark");
+
+            JSONObject objPositions= (JSONObject) ((JSONObject) new JSONParser().parse(new FileReader("src/JSON2SQL/dist_locations.json"))).get("position");
+
+            HashMap<String, HashMap> positions = new HashMap<>();
+            positions.putAll(objPositions);
+
+
+            /* Experimental portion. Needs further work to correctly import positions.
+
+            for (Map.Entry<String, HashMap> entryPositions : locations.entrySet()) {
+                String locPosLatitude = (String) entryPositions.getValue().get("latitude");
+                String locPosLongitude = (String) entryPositions.getValue().get("longitude");
+                System.out.println(locPosLatitude + locPosLongitude);
+            }*/
+
+            System.out.println("(\"" + locName + "\", \"" + locDesc + "\", \"" + locFloor + "\", \"" + locLandmark + "\"),");
+
+        }
+    }
+}
+
